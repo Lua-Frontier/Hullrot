ebc44f47b0a75749bc70af57f55c8e3db905e8bd
HacksLua <parkyrist.ak@gmail.com>
2026-03-29 22:13:34 +0700

    temp publish

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -68,10 +68,9 @@ def get_files_to_publish() -> Iterable[str]:
 
 
 def get_engine_version() -> str:
-    proc = subprocess.run(["git", "describe","--tags", "--abbrev=0"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8")
-    tag = proc.stdout.strip()
-    assert tag.startswith("v")
-    return tag[1:] # Cut off v prefix.
+    branch = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8").stdout.strip()
+    commit = subprocess.run(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8").stdout.strip()
+    return f"{branch}-{commit}"
 
 
 if __name__ == '__main__':
